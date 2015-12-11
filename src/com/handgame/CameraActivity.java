fc24abee620a545731eb188cd98c6a61a29c233f
yippee-ki-yay <nesa993@gmail.com>
2015-12-11 01:10:39 +0100

    dodao meni i prebacio cameru u poseban activity takodje odradio mali treshold na frejmovima kamere

---
+++ src/com/handgame/CameraActivity.java
@@ -0,0 +1,117 @@
+package com.handgame;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class CameraActivity extends Activity implements CvCameraViewListener2
+{
+
+private JavaCameraView mCameraView = null;
+private Mat result = null;
+	
+	//anonimna callback klasa koristimo kad se konektujemo na opencv
+	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this)
+	{
+		@Override
+		public void onManagerConnected(int status)
+		{
+			switch(status)
+			{
+				case LoaderCallbackInterface.SUCCESS:
+				{
+					mCameraView.enableView(); //ako se konektovao za cv enablujemo kameru
+					break;
+				}
+				default:
+				{
+					super.onManagerConnected(status);
+				}
+			}
+		}
+	};
+	
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		//da odemo fullscreen
+		 requestWindowFeature(Window.FEATURE_NO_TITLE);
+		    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+		                            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		setContentView(R.layout.activity_camera);
+
+		//nalazimo nas camera widget i ukljucimo da je vidljiv
+		mCameraView = (JavaCameraView)findViewById(R.id.handgame_surface_view);
+		mCameraView.setVisibility(SurfaceView.VISIBLE);
+				
+		//postavimo da je listener za cameru ova klasa jer i mi u ovoj klasi imeplemtiramo camera listener
+		mCameraView.setCvCameraViewListener(this);
+
+	}
+
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		
+		//just as our app is loaded we call a helper class to bind activity to opencv service
+		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+	}
+
+
+	@Override
+	public void onCameraViewStarted(int width, int height) {
+		// TODO Auto-generated method stub
+		result = new Mat();
+		
+	}
+
+
+	@Override
+	public void onCameraViewStopped() {
+		// TODO Auto-generated method stub
+		result.release();
+		
+	}
+
+
+	@Override
+	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+		
+		//u sustini ovde dobijamo ulazni frame i mogli bi raditi neke manipulacije
+		
+		//treshold odradjen kao primer ali i odmah se vidi da ce nam trebati ovo za izvalcenje sake
+		Imgproc.adaptiveThreshold(inputFrame.gray(), result, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 15, 4);
+		
+		return result;
+	}
+	
+	@Override
+	public void onDestroy()
+	{
+		super.onDestroy();
+		
+		if(mCameraView != null)
+		{
+			mCameraView.disableView();
+		}
+	}
+
+
+
+}
--- src/com/handgame/MainActivity.java
+++ src/com/handgame/MainActivity.java
@@ -1,100 +1,56 @@
 package com.handgame;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
-import org.opencv.android.JavaCameraView;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Mat;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.SurfaceView;
+import android.os.Process;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity implements CvCameraViewListener2
+/**
+ * This is the entry point of the app for now it's just showing a basic menu
+ * 
+ * @author Nenad Palinkasevic
+ *
+ */
+public class MainActivity extends Activity
 {
 
-	private JavaCameraView mCameraView = null;
-	
-	//anonimna callback klasa koristimo kad se konektujemo na opencv
-	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this)
-	{
-		@Override
-		public void onManagerConnected(int status)
-		{
-			switch(status)
-			{
-				case LoaderCallbackInterface.SUCCESS:
-				{
-					mCameraView.enableView(); //ako se konektovao za cv enablujemo kameru
-					break;
-				}
-				default:
-				{
-					super.onManagerConnected(status);
-				}
-			}
-		}
-	};
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		//nalazimo nas camera widget i ukljucimo da je vidljiv
-		mCameraView = (JavaCameraView)findViewById(R.id.handgame_surface_view);
-		mCameraView.setVisibility(SurfaceView.VISIBLE);
 		
-		//postavimo da je listener za cameru ova klasa jer i mi u ovoj klasi imeplemtiramo camera listener
-		mCameraView.setCvCameraViewListener(this);
+		Button calibrationButton = (Button)findViewById(R.id.CalibrationButton);
+		Button exitButton = (Button)findViewById(R.id.ExitButton);
+		
+		
+		calibrationButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i =  new Intent(MainActivity.this, CameraActivity.class);
+				startActivity(i);
+				
+			}
+		});
+		
+		
+		exitButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Process.killProcess(Process.myPid());
+				System.exit(0);
+				
+			}
+		});
 		
 	}
 
 	
-	@Override
-	public void onResume()
-	{
-		super.onResume();
-		
-		//just as our app is loaded we call a helper class to bind activity to opencv service
-		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
-	}
 
-
-	@Override
-	public void onCameraViewStarted(int width, int height) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Override
-	public void onCameraViewStopped() {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Override
-	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-		
-		//u sustini ovde dobijamo ulazni frame i mogli bi raditi neke manipulacije
-		
-		return inputFrame.gray();
-	}
-	
-	@Override
-	public void onDestroy()
-	{
-		super.onDestroy();
-		
-		if(mCameraView != null)
-		{
-			mCameraView.disableView();
-		}
-	}
 
 }
